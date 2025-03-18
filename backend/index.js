efa7ded20cbeaf8ec2b8e018f37af3657d37219d
Arpit Joshi <arpitjoshi2004@gmail.com>
2025-03-19 01:47:59 +0530

    add build configuration and leave room functionality

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import path from 'path';
 
 
 const app = express();
@@ -38,13 +39,51 @@ io.on("connection", (socket) => {
 
         rooms.get(roomId).add(userName);
         io.to(roomId).emit('userJoined', Array.from(rooms.get(roomId)));
-        console.log('user joined room:', roomId, userName);
+
+
     });
 
+    socket.on('codeChange', ({ roomId, code }) => {
+        socket.to(roomId).emit('codeUpdate', code);
+    });
+
+    socket.on('leaveRoom', () => {
+        if (currentRoom && currentUser) {
+            rooms.get(currentRoom).delete(currentUser);
+            io.to(currentRoom).emit('userJoined', Array.from(rooms.get(currentRoom)));
+
+            socket.leave(currentRoom);
+            currentRoom = null;
+            currentUser = null;
+        }
+    })
+
+    socket.on('disconnect', () => {
+        if (currentRoom && currentUser) {
+            rooms.get(currentRoom).delete(currentUser);
+            io.to(currentRoom).emit('userJoined', Array.from(rooms.get(currentRoom)));
+        }
+        console.log('a user disconnected', socket.id);
+    });
+
+    socket.on('typing', ({ roomId, userName }) => {
+        socket.to(roomId).emit('userTyping', userName);
+    });
+
+    socket.on('languageChange', ({ roomId, language }) => {
+        socket.to(roomId).emit('languageUpdate', language);
+    });
 
 });
 
 const port = 3000;
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html")); //to run our frontend on the backend url kinda
+});
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
